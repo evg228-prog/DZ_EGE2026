3c6f7d1dd67342e24ff5365a3b61b9a05de0f091
Genya <bornb228@gmail.com>
2025-12-19 22:20:00 +0200

    ...

--- DZ_Task-24/1273-perebor.py
+++ DZ_Task-24/1273-perebor.py
@@ -3,7 +3,7 @@ with open(r'.\files\24_1273 (1).txt') as file:
 
 ans = 0
 cnt = 2
-for i in range(len(data) -2):
+for i in range(len(data) - 2):
     if data[i:i + 3] != 'XYZ':
         cnt += 1
     else:
