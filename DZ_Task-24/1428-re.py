f9812e91815695e9f3095e7ad55c196f4b7860ca
Genya <bornb228@gmail.com>
2025-12-20 17:12:30 +0200

    ...

--- DZ_Task-24/1428-re.py
+++ DZ_Task-24/1428-re.py
@@ -2,8 +2,10 @@ from re import *
 with open(r'.\files\24_1428.txt') as file:
     data = file.readline()
 
-pattern = r'(.[^XY].[^XZ].)+'
-matches = [match.group() for match in finditer(pattern, data)]
-print(len(max(matches, key=len)))
-
+pattern = r'(^|(?<=(XY|XZ))).+?($|(?=(XY|XZ)))'
+ans = 0
+for match in finditer(pattern, data):
+    cnt_XY_XZ = match.groups().count('XY') + match.groups().count('XZ')
+    ans = max(ans, len(match.group()) + 2 if cnt_XY_XZ else 1)
+print(ans)
 # 25
--- DZ_Task-24/4710-re.py
+++ DZ_Task-24/4710-re.py
@@ -2,7 +2,8 @@ from re import *
 with open(r'.\files\24_4710.txt') as file:
     data = file.readline()
 
-pattern = r'([CDF][AO])+'
+vow = 'EYUIOA'
+pattern = Fr'([^{vow}][{vow}])+'
 matches = [match.group() for match in finditer(pattern, data)]
 print(len(max(matches, key=len)) // 2)
 
