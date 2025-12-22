32ebdb6570a9824382e23a3ed4d27669787c529a
Genya <bornb228@gmail.com>
2025-12-22 13:40:29 +0200

    ...

--- DZ_Task-8/18976.py
+++ DZ_Task-8/18976.py
@@ -3,8 +3,13 @@ from string import printable
 
 cnt = 0
 for val in product(printable[:20], repeat=5):
-    if val[0] != '0' and sum(map(int, val[-2:])) == 26:
-        val = ''.join(['*' if i in '02468ACEGI' else '+' for i in val])
-        if val == '+*+*+' or val == '*+*+*':
-            cnt += 1
+    if val[0] != '0':
+        val_1 = int(val[0], 20)
+        val_5 = int(val[-1], 20)
+        if val_1 + val_5 == 26:
+            val = ''.join(['*' if i in '02468acegi' else '+' for i in val])
+            if val == '+*+*+' or val == '*+*+*':
+                cnt += 1
 print(cnt)
+
+# 13000
